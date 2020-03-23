1b388323a5ad66f8475c8d553979c94abfa79f58
RPIUdishan <it18082234@my.sliit.lk>
2020-03-23 09:53:04 +0530

    fixed fully completed

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express';
 import { SocketDotIO } from 'socket.io';
+import { addBook } from '../controllers/admin.controller';
 
 export const adminRoutes = (app: Application, io: SocketIO.Server) => {
-  app.route('/api/v1/admin')
+  app.route('/api/v1/admin/book')
+  .post(addBook)
     
 }//test commit
