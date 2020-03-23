05f3ca3c4d4748720545c99177deb3bf9f53cad9
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2020-03-23 17:45:47 +0530

    sign in

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,20 @@ import { Application, Request, Response } from "express";
 import Mysql from '../config/db';
 import { FieldInfo, MysqlError } from "mysql";
 
+export const login = (req: Request, res: Response) => {
+   const get_user_query = "select pwd,privilege from users where email=?";
+   // Example query that you can execute to get all the users from database
+   Mysql.getPool().query(get_user_query, [req.query.email], (err: any, results: any) => {
+      if (err) {
+         console.log("Error", err);
+         res.status(500)
+            .json({ "error": err });
+      } else {
+         console.log("Result: ", results);
+         res.json(results);
+      }
+   });
+}
 export const addUser = (req: Request, res: Response) => {
    // Example query that you can execute to get all the users from database
    const add_user_query = "insert into users(`email`, `name`,`pwd`) values (?,?,?)";
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,15 @@
 import { Application } from 'express';
 import { SocketDotIO } from 'socket.io';
-import { getUser,addUser, updateUser, addProfilePic } from '../controllers/user.controller';
+import { login,getUser,addUser, updateUser, addProfilePic } from '../controllers/user.controller';
 
 export const userRoutes = (app: Application, io: SocketIO.Server) => {
+  app.route('/api/v1/user/login')
+  .get(login)
   app.route('/api/v1/user')
   .get(getUser)
   .post(addUser)
   .put(updateUser);
+
   app.route('/api/v1/user/profile_pic')
   .put(addProfilePic);
 }//test commit
