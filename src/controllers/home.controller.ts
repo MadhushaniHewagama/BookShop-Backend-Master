77c12e038bdbfc96928d7358440fed1a88cdbeec
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2020-04-06 00:39:39 +0530

    fixed

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -15,3 +15,50 @@ import { FieldInfo, MysqlError } from "mysql";
       }
    });
  }
+
+
+  export const getCart = (req: Request, res: Response) => {
+    const get_user_query = "select b.title,c.quantity,b.selling_price from cart_product c left outer join books b on c.bookID=b.bookID where c.email=? and c.status='0'";
+    // Example query that you can execute to get all the users from database
+    Mysql.getPool().query(get_user_query, [req.query.email], (err: any, results: any) => {
+       if (err) {
+          console.log("Error", err);
+          res.status(500)
+             .json({ "error": err });
+       } else {
+          console.log("Result: ", results);
+          res.json(results);
+       }
+    });
+ }
+ 
+ export const getTotal = (req: Request, res: Response) => {
+    const get_user_query = "select  sum(b.selling_price * c.quantity) as total from cart_product c left outer join books b on c.bookID=b.bookID where c.email=? and c.status='0'";
+    // Example query that you can execute to get all the users from database
+    Mysql.getPool().query(get_user_query, [req.query.email], (err: any, results: any) => {
+       if (err) {
+          console.log("Error", err);
+          res.status(500)
+             .json({ "error": err });
+       } else {
+          console.log("Result: ", results);
+          res.json(results);
+       }
+    });
+ }
+ 
+ export const addCart = (req: Request, res: Response) => {
+    // Example query that you can execute to get all the users from database
+    const add_user_query = "insert into cart_product(`bookID`, `quantity`,`email`) values (?,?,?)";
+    Mysql.getPool().query(add_user_query, [req.body.bookID, req.body.quantity, req.body.email], (err: any, results: any) => {
+       if (err) {
+          console.log("Error", err);
+          res.status(500)
+             .json({ "error": err });
+       } else {
+          console.log("Result: ", results);
+          res.json(results);
+       }
+    }
+    );
+ }
--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -2,10 +2,15 @@
 
 import { Application } from 'express';
 import { SocketDotIO } from 'socket.io';
-import { getBooks } from '../controllers/home.controller';
+import { getBooks,getCart ,getTotal,addCart} from '../controllers/home.controller';
 
 export const homeRoutes = (app: Application, io: SocketIO.Server) => {
   
   app.route('/api/v1/home')
     .get(getBooks);
+app.route('/api/v1/cart')
+    .get(getCart)
+    .post(addCart)
+    app.route('/api/v1/cart/total')
+    .get(getTotal)
 }//test commit
